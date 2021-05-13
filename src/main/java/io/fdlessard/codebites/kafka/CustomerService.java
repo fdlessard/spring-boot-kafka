4d84411f02ff55ea95de1de3415719a0b66383df
Francois-David Lessard <francois-david.lessard@nbc.ca>
2021-05-13 10:02:52 -0400

    Added ids in payload

---
+++ src/main/java/io/fdlessard/codebites/kafka/CustomerService.java
@@ -0,0 +1,21 @@
+package io.fdlessard.codebites.kafka;
+
+import io.fdlessard.codebites.kafka.model.Customer;
+import io.fdlessard.codebites.kafka.repositories.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CustomerService {
+
+  private CustomerRepository customerRepository;
+
+  public Customer createCustomer(Customer customer) {
+
+    Customer createdCustomer = customerRepository.save(customer);
+
+    return createdCustomer;
+  }
+
+}
